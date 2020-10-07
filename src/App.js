ac8efa6acd3dd32fc943609391a6d241a2217c33
Ivan Dominguez <ridoming@lakeheadu.ca>
2020-10-06 20:48:15 -0500

    added Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import  {Route, BrowserRouter as Router, Link, Switch} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 
 function App() {
   return (
-    <div>First Commit</div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+    
   )
 }
 
